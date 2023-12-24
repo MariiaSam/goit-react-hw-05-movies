518a0ded85d7889abb85f7017d76576ba9e3eb4f
MariiaSam <kontragent@ukr.net>
2023-12-24 13:22:59 +0200

    corr

--- src/components/MovieList/MovieList.styled.jsx
+++ src/components/MovieList/MovieList.styled.jsx
@@ -10,9 +10,6 @@ export const MovieListt = styled.ul`
 
 export const Item = styled.li`
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.7);
-  border-radius: 5px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
   padding: 10px;
@@ -21,7 +18,9 @@ export const Item = styled.li`
 export const Img = styled.img`
   display: block;
   height: auto;
-  max-width: 100%;
+  width: auto;
+  border-radius: 5px;
+
 `;
 
 export const MovieTitle = styled.h1`
